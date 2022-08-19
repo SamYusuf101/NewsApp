4c75bda87228e88b48b8d142f3d2ec7fba05fc70
Sam Yusuf <samyusuf101@gmail.com>
2022-08-19 21:33:25 +0100

    Update testing.jsx

--- Screens/testing.jsx
+++ Screens/testing.jsx
@@ -19,17 +19,8 @@ const DetailsHeader = ({data, navigation}) => (
   style={{width: '100%', height:'100%'}}
   />
 
-  <CircleButton
-  imgUrl={assets.left}
-  handlePress={() => navigation.goBack()}
-  left={15}
-  top={StatusBar.currentHeight + 10}
-  />
-  <CircleButton
-  imgUrl={assets.heart}
-  right={15}
-  top={StatusBar.currentHeight + 10}
-  />
+ 
+  
   </View>
 
 )
