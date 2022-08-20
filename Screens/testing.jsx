2b5e1702df50f501a58160ed544c40261e9b17dd
Sam Yusuf <samyusuf101@gmail.com>
2022-08-20 23:14:04 +0100

    Update testing.jsx

--- Screens/testing.jsx
+++ Screens/testing.jsx
@@ -29,21 +29,8 @@ const Details = ({ route, navigation }) => {
   const { data } = route.params;
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <FocusedStatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent={true}
-      />
-      <View style={{
-        width: '100%',
-        position: 'absolute',
-        bottom: 0,
-        paddingVertical: SIZES.font,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(255,255,255,0.5)',
-        zIndex: 1
-      }}>
+      
+      
         <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
 
       </View>
