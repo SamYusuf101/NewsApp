e7c2e723674c75977267fedcfe44b7a88bf87254
Sam Yusuf <samyusuf101@gmail.com>
2022-10-16 17:19:46 +0100

    Update TopNavigation.js

--- Components/TopNavigation.js
+++ Components/TopNavigation.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+7import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import React, { useContext } from 'react';
 import {AntDesign, MaterialCommunityIcons, SimpleLineIcons} from "@expo/vector-icons";
 import { NewsContext } from '../API/Context';
@@ -38,7 +38,7 @@ const TopNavigation = ({ index, setIndex }) => {
         onPress={() => fetchNews("general")}
         >
         <Text style ={styles.text}>
-        <AntDesign name="reload1" size={24} color="#82EEFD"/>
+        <AntDesign name="reload1" size={22} color="#82EEFD"/>
         </Text>
         </TouchableOpacity>
       ) : (
